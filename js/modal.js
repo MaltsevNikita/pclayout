bf952e3ab791df64882ceb81e835c2ec2b5bf280
DESKTOP-OMV7VHD\Redmi <mn121288@gmail.com>
2025-11-11 19:50:04 +0300

    lesson-2 done

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+const modalBtn = document.querySelector('.modal__button')
+const modal = document.querySelector('.modal')
+
+modalBtn.addEventListener('click', () => {
+    modal.style.display = 'flex'
+    // console.dir(modal)
+})
+
+modal.addEventListener('click', (event) => {
+    const modalContent = event.target.closest('.modal__inner')
+    console.log(modalContent);
+    if(!modalContent) {
+        modal.style.display = 'none'
+    }
+})
